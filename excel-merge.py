28bae8bcc446036af17f6655b9ed3105884f6c86
zc910704 <329188614@qq.com>
2018-12-25 23:10:48 +0800

    修改逻辑

--- excel-merge.py
+++ excel-merge.py
@@ -21,24 +21,27 @@ def verify_file(path):
     return False
 
 
-def readTable(filePath):
+def readTable(filePath, father_dir):
     data = pd.read_excel(filePath)
     df = pd.DataFrame(columns=data.iloc[1])
     data.columns = data.iloc[1]
     for index,row in data.iterrows():
         if re.match('\d+',str(row['项目编码'])):
             df = df.append(row)
-    return df.loc[:, [column for column in df.columns if str(column) != 'nan']]
-
+    res = df.loc[:, [column for column in df.columns if str(column) != 'nan']]
+    res['单位工程'] = father_dir
+    return res
 
 def traverse(BASE_DIR):
+    father_dir = re.split(r'[\\\/]', BASE_DIR)[-1]
+    print(father_dir)
     files = os.listdir(BASE_DIR)
     for file in files:
         tmp_path = os.path.join(BASE_DIR, file)
         if not os.path.isdir(tmp_path):
             if verify_file(tmp_path):
                 print('file found')
-                res = readTable(tmp_path)
+                res = readTable(tmp_path, father_dir)
                 result_list.append(res)
         else:
             traverse(tmp_path)
